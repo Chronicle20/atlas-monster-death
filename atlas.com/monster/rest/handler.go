feac1a9f73b1cfc440469bce1988098f534a8344
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-28 07:16:16 -0500

    Migrate to a maintained version of manyminds/api2go jsonapi.

--- atlas.com/monster/rest/handler.go
+++ atlas.com/monster/rest/handler.go
@@ -3,7 +3,7 @@ package rest
 import (
 	"context"
 	"github.com/Chronicle20/atlas-rest/server"
-	"github.com/manyminds/api2go/jsonapi"
+	"github.com/jtumidanski/api2go/jsonapi"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
